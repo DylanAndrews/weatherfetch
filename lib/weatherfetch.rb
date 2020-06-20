0d5e7e2f87afe474104f6f3b88916ac4b65caf24
Dylan Andrews <dylanandr@gmail.com>
2020-06-20 16:03:14 -0500

    Attempt to get thor working with simple city command

--- lib/weatherfetch.rb
+++ lib/weatherfetch.rb
@@ -1,8 +1,12 @@
 require 'httparty'
+require 'thor'
+require 'pry'
 
 module WeatherFetch
-  def foo
-    options = { query: { q: 'nashville', appid: 'c8d7f5fd25b8914cc543ed45e6a40bba' }}
-    r = HTTParty.get('http://api.openweathermap.org/data/2.5/weather', options)
+  class CLI < Thor
+    def city(city)
+      options = { query: { q: city, appid: 'c8d7f5fd25b8914cc543ed45e6a40bba' } }
+      r = HTTParty.get('http://api.openweathermap.org/data/2.5/weather', options)
+    end
   end
 end
